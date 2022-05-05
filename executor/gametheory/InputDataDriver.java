06c10d1cade31b0aa0d67fc1ba9c0186ea844cd3
tung-dnt <tungdnt779901@gmail.com>
2022-05-06 00:37:17 +0700

    Add traffic control problem

--- executor/gametheory/InputDataDriver.java
+++ executor/gametheory/InputDataDriver.java
@@ -3,6 +3,7 @@ package gametheory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -37,16 +38,16 @@ public class InputDataDriver {
 		List<Conflict> conflictSet = new ArrayList<>();
 
 		if (isRowEmpty(row)) {
-			System.out.println("Empty");
+			System.out.println("No Conflict Set");
 			return null;
 		}
 
 		while (!isRowEmpty(row)) {
 			int col = 0;
-			
-			while (getStringOfCoordinate(row, col) != null) {
+
+			while (isCellExist(row, col)) {
 				String s = getStringOfCoordinate(row, col);
-				if (!s.equals("")) {
+				if (s != null) {
 					Conflict conflict = new Conflict(s);
 					conflictSet.add(conflict);
 				}
@@ -57,36 +58,46 @@ public class InputDataDriver {
 		return conflictSet;
 	}
 
-	public NormalPlayer setNormalPlayer(int row, int numberOfProperties) throws IOException {
-		int strategiesOfPlayer = getValueOfCoordinate(row, 0);
+	private boolean isCellExist(int rowIndex, int columnIndex) throws IOException {
+		Row row = sheet.getRow(rowIndex);
+		Cell cell = row.getCell(columnIndex);
+		file.close();
+		return cell != null;
+	}
+
+	public NormalPlayer setNormalPlayer(int row, int numberOfProperties, List<Double> weights) throws IOException {
+		int strategiesOfPlayer = getValueOfCoordinate(row, 0).intValue();
 		List<Strategy> strategies = new ArrayList<>(strategiesOfPlayer);
 		int column = 1;
 		for (int i = 0; i < strategiesOfPlayer; i++) {
 			Strategy strategy = new Strategy();
-			
 			for (int j = 0; j < numberOfProperties; j++) {
 				strategy.addProperty(getValueOfCoordinate(row, column));
 				column++;
 			}
+			strategy.setPayoff(weights);
 			strategies.add(strategy);
 		}
 		return new NormalPlayer(strategies);
 	}
 
-	public Integer getValueOfCoordinate(int rowIndex, int columnIndex) throws IOException {
+	public Double getValueOfCoordinate(int rowIndex, int columnIndex) throws IOException {
+		DataFormatter formatter = new DataFormatter();
 		Row row = sheet.getRow(rowIndex);
 		Cell cell = row.getCell(columnIndex);
 		if (cell == null) {
 			System.err.println("There is an empty cell");
 			return null;
 		}
+
 		file.close();
-		return (int) cell.getNumericCellValue();
+		return Double.parseDouble(formatter.formatCellValue(cell));
 	}
 
 	public String getStringOfCoordinate(int rowIndex, int columnIndex) throws IOException {
 		Row row = sheet.getRow(rowIndex);
 		Cell cell = row.getCell(columnIndex);
+
 		if (cell == null) {
 			System.err.println("There is an empty cell");
 			return null;
@@ -119,6 +130,7 @@ public class InputDataDriver {
 	}
 
 	public static void displayNormalPlayerList(List<NormalPlayer> normalPlayerList) {
+		System.out.println();
 		for (NormalPlayer normalPlayer : normalPlayerList) {
 			System.out.println("Normal player : " + (normalPlayerList.indexOf(normalPlayer) + 1));
 			normalPlayer.display();
