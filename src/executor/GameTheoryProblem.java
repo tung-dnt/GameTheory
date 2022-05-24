eb89da974202c035f090f076d97e895e951e4392
tung-dnt <tungdnt779901@gmail.com>
2022-05-24 11:33:19 +0700

    Update Documents

--- src/executor/GameTheoryProblem.java
+++ src/executor/GameTheoryProblem.java
@@ -1,12 +1,13 @@
 package executor;
 
-/*STEPS TO FIND OUT THE BEST RESPONSE/NASH EQUILIBRIUM
-        *1)Set up the game with `load()` method
-        *2)Define each player's set of strategies with `loadNormalPlayersFromFile()`
-        *3)Find and eliminate dominant strategies with `eliminateDominantStrategies()`
-        *4)Find the pure strategy Nash Equilibria of the game
-        *5)Find the mixed strategy Nash Equilibrium of the game
-**/
+/**
+ * @General_Formula: STEPS TO FIND OUT THE BEST RESPONSE/NASH EQUILIBRIUM
+ * 1)Set up the game with `load()` method
+ * 2)Define each player's set of strategies with `loadNormalPlayersFromFile()`
+ * 3)Find and eliminate dominant strategies with `eliminateDominantStrategies()`
+ * 4)Find the pure strategy Nash Equilibria of the game
+ * 5)Find the mixed strategy Nash Equilibrium of the game
+ **/
 //--------------------------------------------------------------------------
 /* *
  * WARNING: DO NOT CHANGE ORDER OF GameTheoryProblem CONSTRUCTOR
@@ -49,7 +50,7 @@ public class GameTheoryProblem implements Problem {
     private SpecialPlayer specialPlayer;
     private List<NormalPlayer> normalPlayers;
     private List<Conflict> conflictSet;
-    //Store average pure payoff diff in descending order
+    //Store average pure payoff differences
     private List<Double> playerAvgDiffs;
 
     public GameTheoryProblem(String path, int startRow) throws IOException {
@@ -63,15 +64,22 @@ public class GameTheoryProblem implements Problem {
         eliminateConflictStrategies();
     }
 
+    /**
+     * @usage To set gametheory instance data from .xlsx file
+     * @modifies specialPlayer, normalPlayers, conflictSet
+     * @params String path: .xlsx pathname,
+     * ________int startRow: start row in xlsx file - where real data inserted
+     */
     private void load(String path, int startRow) throws IOException {
         startRow--; //Since row index start from 0 --> index = startRow-1
         InputDataDriver driver = new InputDataDriver(path);
         // WARNING: Must not modify these variables
         boolean isSpecialPlayerExist = driver.getValueOfCoordinate(startRow, 0) != 0;
+        // SET UP DATA LOCATION
         int NORMAL_PLAYER_START_ROW = isSpecialPlayerExist ? startRow + 3 : startRow + 1;
         int numberOfNP = driver.getValueOfCoordinate(NORMAL_PLAYER_START_ROW, 0).intValue();
         int CONFLICT_SET_START_ROW = isSpecialPlayerExist ? startRow + 5 + numberOfNP : startRow + 3 + numberOfNP;
-
+        // Load Special Player
         if (isSpecialPlayerExist) {
             specialPlayer = driver.loadSpecialPlayerFromFile(startRow);
             specialPlayer.displayInf();
@@ -82,6 +90,14 @@ public class GameTheoryProblem implements Problem {
         conflictSet = driver.loadConflictSetFromFile(CONFLICT_SET_START_ROW);
     }
 
+    /**
+     * @usage To remove all conflict strategies of conflict set declared in .xlsx file
+     * @modifies normalPlayers
+     * @algorithm Loop through conflict set
+     * -----------> set all strategies matching with strategies in conflict set to null
+     * -----------> remove all null strategies in normalPlayers
+     * Conflict set format: Left Player, Left Player Strategy, Right Player, Right Player Strategy
+     */
     private void eliminateConflictStrategies() {
         if (conflictSet != null) {
             for (Conflict conflict : conflictSet) {
@@ -106,6 +122,13 @@ public class GameTheoryProblem implements Problem {
         }
     }
 
+    /**
+     * @usage To get the smallest average pure payoff difference among players
+     * @algorithm 1) Loop through normalPlayers and calculate player average differences
+     * ______________with formula |playerPayoff - opponentPayoff| / normalPlayers.size()
+     * ___________2) Save result in a list
+     * ___________3) Assign result list to playerAvgDiffs property
+     */
     private double computeNashEquilibrium() {
         double nash;
         List<Double> playerAvgDiffs = new ArrayList<>();
@@ -133,28 +156,54 @@ public class GameTheoryProblem implements Problem {
         return normalPlayers;
     }
 
+    /**
+     * @usage To get player index with the highest pure payoff
+     * @algorithm 1) Save all pure payoff values to a list
+     * ___________2) Get max value in payoffs list
+     * ___________3) Return max value index in list (Since pure payoff index == player index)
+     */
     public int getDominantPlayerIndex() {
-        int bestResponse = 0;
         List<Double> payoffs = new ArrayList<>();
-
-        for (NormalPlayer p : normalPlayers) {
-            payoffs.add(p.getPurePayoff());
-        }
-
-        double max = normalPlayers.get(0).getPurePayoff();
-
-        for (int i = 0; i < payoffs.size(); i++) {
-            if (payoffs.get(i) > max) {
-                bestResponse = i;
-            }
-            max = normalPlayers.get(i).getPurePayoff();
-        }
-        return bestResponse;
+        normalPlayers.forEach(player -> payoffs.add(player.getPurePayoff()));
+        double max = Collections.max(payoffs);
+        return payoffs.indexOf(max);
     }
 
+    /**
+     * @usage Get user with best response strategy
+     * ----> The lower payoff average difference, the more equilibrium strategy is
+     */
     public int getNashEquiPlayerIndex() {
         computeNashEquilibrium();
         return playerAvgDiffs.indexOf(Collections.min(playerAvgDiffs));
+    }
+
+    /**
+     * @usage To set up evaluator for MOEA model every single time Evaluation run
+     */
+    @Override
+    public void evaluate(Solution solution) {
+        boolean[] areObjectivesExist = EncodingUtils.getBinary(solution.getVariable(0));
+        double[] NashEquilibrium = {computeNashEquilibrium()};
+        double[] payoffs = new double[solution.getNumberOfObjectives()];
+
+        for (int i = 0; i < normalPlayers.size(); ++i) {
+            if (areObjectivesExist[i]) {
+                payoffs[i] = normalPlayers.get(i).getBestResponse();
+            }
+        }
+        solution.setObjectives(payoffs);
+        solution.setConstraints(NashEquilibrium);
+    }
+
+    // SOLUTION = VARIABLE -> OBJECTIVE || CONSTRAINT
+    @Override
+    public Solution newSolution() {
+        //Number of strategies each player
+        int numbeOfNP = normalPlayers.size();
+        Solution solution = new Solution(1, numbeOfNP, 1);
+        solution.setVariable(0, EncodingUtils.newBinary(numbeOfNP));
+        return solution;
     }
 
     public String toString() {
@@ -188,31 +237,6 @@ public class GameTheoryProblem implements Problem {
     public int getNumberOfConstraints() {
         // TODO Auto-generated method stub
         return conflictSet.size();
-    }
-
-    @Override
-    public void evaluate(Solution solution) {
-        boolean[] areObjectivesExist = EncodingUtils.getBinary(solution.getVariable(0));
-        double[] NashEquilibrium = {computeNashEquilibrium()};
-        double[] payoffs = new double[solution.getNumberOfObjectives()];
-
-        for (int i = 0; i < normalPlayers.size(); ++i) {
-            if (areObjectivesExist[i]) {
-                payoffs[i] = normalPlayers.get(i).getBestResponse();
-            }
-        }
-        solution.setObjectives(payoffs);
-        solution.setConstraints(NashEquilibrium);
-    }
-
-    // SOLUTION = VARIABLE -> OBJECTIVE || CONSTRAINT
-    @Override
-    public Solution newSolution() {
-        //Number of strategies each player
-        int numbeOfNP = normalPlayers.size();
-        Solution solution = new Solution(1, numbeOfNP, 1);
-        solution.setVariable(0, EncodingUtils.newBinary(numbeOfNP));
-        return solution;
     }
 
     @Override
