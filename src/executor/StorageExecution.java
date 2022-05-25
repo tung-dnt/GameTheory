1bd3def02fe187aeb1ba74fb343d239f4d3d7120
tung-dnt <tungdnt779901@gmail.com>
2022-05-25 22:38:42 +0700

    Update Input Handler

--- src/executor/StorageExecution.java
+++ src/executor/StorageExecution.java
@@ -1,5 +1,6 @@
 package executor;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -8,7 +9,10 @@ import org.moeaframework.core.NondominatedPopulation;
 
 public class StorageExecution {
     public static void main(String[] args) throws IOException {
-        GameTheoryProblem problem = new GameTheoryProblem("TrafficControl.xlsx", 17);
+        String inputFile = "TrafficControl.xlsx";
+        File file = new File(System.getProperty("user.dir")+"/input/"+inputFile);
+
+        GameTheoryProblem problem = new GameTheoryProblem(file.getAbsolutePath(), 2);
         // solve using NSGA-II
         NondominatedPopulation results = new Executor()
                 .withProblem(problem)
@@ -35,7 +39,7 @@ public class StorageExecution {
         System.out.println("==================================\nMOST DOMINANT SOLUTION:");
         System.out.printf("Normal Player %d - Strategy %d\n", outstandingPlayerIndex + 1, bestRes + 1);
         System.out.printf("Payoff: %f\n", outstandingPlayer.getStrategyAt(bestRes).getPayoff());
-        System.out.printf("Properties of best strategy: %s\n\n", outstandingPlayer.getStrategyAt(bestRes));
+        System.out.printf("Properties of dominant strategy: %s\n\n", outstandingPlayer.getStrategyAt(bestRes));
     }
 
     private static void printEquilibriaStrategy(GameTheoryProblem problem, NondominatedPopulation result) {
@@ -48,6 +52,6 @@ public class StorageExecution {
         System.out.print("==================================\nBEST RESPONSE:\n");
         System.out.printf("Normal Player %d - Strategy %d\n", equiPlayerIndex + 1, bestRes + 1);
         System.out.printf("Payoff: %f\n", player.getStrategyAt(bestRes).getPayoff());
-        System.out.printf("Properties of best strategy: %s\n\n", player.getStrategyAt(bestRes));
+        System.out.printf("Properties of best response: %s\n\n", player.getStrategyAt(bestRes));
     }
 }
